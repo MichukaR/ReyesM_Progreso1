37bf5c535921d2369b6ba750a7a9edb679707425
Michu <michelle.reyes.delgado@udla.edu.ec>
2025-04-29 16:38:55 -0500

    Tipos de letra / Botón Calcular

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -9,11 +9,17 @@ public class Reserva
     [Key]
     [MaxLength(10)]
     public int id { get; set; }
-    public DateTime fecha_entrada { get; set; }
-    public DateTime fecha_salida { get; set; }
-    [Required]
-    public double ValorAPagar { get; set; } 
     
+    [Display(Name = "Fecha de ingreso")]
+    public DateTime fecha_entrada { get; set; }
+    
+    [Display(Name = "Fecha de salida")]
+    public DateTime fecha_salida { get; set; }
+
+    [Display(Name = "Valor de la reserva")]
+    public double ValorAPagar  { get; set; }
+    
+    [Display(Name = "Reserva a nombre de:")]
     public string infromacionCliente {get; set;}
     [ForeignKey("informacionCliente")]
     public Cliente? Cliente { get; set; }
